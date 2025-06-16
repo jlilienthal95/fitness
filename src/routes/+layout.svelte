ac8d74c10190427a1ddfc4088734d1ceb90f9707
joggest95 <jilienthal@gmail.com>
2025-06-16 15:25:47 -0400

    basic layout/theme implemented

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,44 @@
 	import '../app.css';
 
 	let { children } = $props();
+
+	const navItemCont = 'flex flex-col items-center justify-between h-[95px]'
+	const navIconCont = "h-[70px] flex items-center"
+	const navLabelClass = ""
 </script>
 
-{@render children()}
+<div class="relative flex md:flex-row flex-col-reverse w-full h-screen bg-[#7a612f]">
+	<div class="absolute inset-0 bg-[url(/strange-bullseyes.png)] opacity-50 z-0">
+
+	</div>
+	<div id="sidebarCont" class="flex md:flex-col md:h-full md:w-20 w-full justify-around items-center z-10 bg-[#7a612f] md:border-r-[1px] md:border-t-0 border-t-[1px]">
+		<div class={navItemCont}>
+			<div class={navIconCont}>
+				<img src="/navIcons/home.png" alt="home" class="h-[50px]"/>
+			</div>
+			<label class={navLabelClass} for="home">Home</label>
+		</div>
+		<div class={navItemCont}>
+			<div class={navIconCont}>
+				<img src="/navIcons/blog.png" alt="blog" class="h-[70px]"/>
+			</div>
+			<label  class={navLabelClass} for="blog">Blog</label>
+		</div>
+		<div class={navItemCont}>
+			<div class={navIconCont}>
+				<img src="/navIcons/insta.png" alt="instagram" class="h-[62px]"/>
+			</div>
+			<label  class={navLabelClass} for="insta">Insta</label>
+		</div>
+		<div class={navItemCont}>
+			<div class={navIconCont}>
+				<img src="/navIcons/email.png" alt="contact" class="h-[58px]"/>
+			</div>
+			<label  class={navLabelClass} for="contact">Contact</label>
+		</div>
+
+	</div>
+	<div class='flex flex-col flex-grow flex-shrink w-full z-1'>
+		{@render children()}
+	</div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,45 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div class="flex w-full md:h-full flex-col items-start md:flex-row gap-10">
+	<div class="hidden md:flex md:h-full w-full flex-col justify-between">
+		<div class="flex flex-row items-center">
+			<img src="/assets/AS-logo-simple-HQ.png" alt="Armstrong Strength Logo" class="md:flex hidden size-30 object-contain hover:saturate-150"/>
+			<div class="oswald-bold w-full text-end text-2xl lg:text-4xl">
+				TRANSFORM YOUR BODY WITH FUNCTIONAL TRAINING
+			</div>
+		</div>
+		<div class="flex w-full h-full justify-end flex-1 flex-row items-center">
+			<div class="lg:w-1/2"></div>
+			<div class="oswald-bold w-full flex-1 text-center text-xl md:static">
+				LIFT DIFFERENT. UNLOCK YOUR REAL-WORLD POTENTIAL WITH FUNCTIONAL TRAINING — FOCUS ON
+				BUILDING STRENGTH THAT TRANSLATES BEYOND THE GYM. INSTEAD OF CHASING EMPTY REPS, TRAIN
+				MOVEMENT PATTERNS THAT MIRROR EVERYDAY TASKS, HELPING YOU LIFT SMARTER, MOVE BETTER, AND
+				FEEL STRONGER ALL DAY, EVERY DAY. IT’S NOT JUST ABOUT LOOKING FIT, IT’S ABOUT BEING CAPABLE.
+			</div>
+		</div>
+	</div>
+	<div class="relative flex h-[calc(100vh-6rem)] md:h-full flex-col md:flex-row overflow-hidden">
+		<!-- <div class="md:hidden absolute top-0 oswald-bold w-full text-end text-2xl lg:text-4xl bg-[#7a612f]/70 p-3">
+			TRANSFORM YOUR BODY WITH FUNCTIONAL TRAINING
+		</div> -->
+		<div class="relative z-10">
+			<img src="/assets/AS-logo-simple-HQ.png" alt="Armstrong Strength Logo" class="md:hidden absolute size-30 object-contain hover:saturate-150"/>
+		</div>
+		<div class="md:hidden absolute bottom-2 w-full flex-1 bg-[#7a612f]/50 bg-opacity rounded-lg p-3 ">
+			<div class="oswald-bold w-full text-lg text-center">
+				LIFT DIFFERENT. UNLOCK YOUR REAL-WORLD POTENTIAL WITH FUNCTIONAL TRAINING — FOCUS ON
+				BUILDING STRENGTH THAT TRANSLATES BEYOND THE GYM. INSTEAD OF CHASING EMPTY REPS, TRAIN
+				MOVEMENT PATTERNS THAT MIRROR EVERYDAY TASKS, HELPING YOU LIFT SMARTER, MOVE BETTER, AND
+				FEEL STRONGER ALL DAY, EVERY DAY. IT’S NOT JUST ABOUT LOOKING FIT, IT’S ABOUT BEING CAPABLE.
+			</div>
+		</div>
+		<video
+
+			muted
+			playsInline
+
+			class="w-full object-cover md:w-200"
+		>
+			<source src="/vids/lifting.mp4" type="video/mp4" />
+			Not supported by your browser.
+		</video>
+	</div>
+</div>
